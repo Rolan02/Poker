b8afa4b8ab9ecc675a05623437788c6fa1f632e4
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2021-07-12 20:04:51 -0400

    Primer commit con la base del proyecto

---
+++ src/Carta.java
@@ -0,0 +1,21 @@
+public class Carta {
+
+    private Tipo tipo;
+    private int valor;
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
