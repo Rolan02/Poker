de46da6ee33fadd9b77acd789b1fbe22a01209d9
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2021-07-12 20:15:36 -0400

    Base del proyecto

---
+++ src/Jugador.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Jugador {
+    private String jugador;
+    private List cartas;
+
+    public String getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(String jugador) {
+        this.jugador = jugador;
+    }
+
+    public List getCartas() {
+        return cartas;
+    }
+
+    public void setCartas(List cartas) {
+        this.cartas = cartas;
+    }
+    public List<Carta> generarMano(){
+        //Debe retornar una lista de  5 cartas aleatorias. ///BRYAN
+    }
+    public Carta cartaMayor(List cartas){
+        //Debe devolder la carta mas alta de la lista. ///SERGIO
+    }
+    public int parCarta(List Cartas){
+        //Buscar si hay un par en la Lista de cartas y devuleve el valor.  ///ROLANDO
+    }
+
+}
---
+++ src/Tipo.java
@@ -0,0 +1,6 @@
+public enum Tipo {
+    ESPADA,
+    CORAZON,
+    TREVOL,
+    DIAMANTE
+}
